806de696f194997fe9db65f4066898eaf9940674
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-02-05 17:26:01 +0200

    Create Simple multiplication.py

---
+++ CodeWars/8kyu/Simple multiplication.py
@@ -0,0 +1,6 @@
+#link for this kata : https://www.codewars.com/kata/583710ccaa6717322c000105/train/python
+def simple_multiplication(n) :
+    if n%2==0:
+        return 8*n
+    else:
+        return 9*n
