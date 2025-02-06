149bf8cd5d9dd223d3fd61e93f194ae381c25e6c
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-02-06 12:16:03 +0200

    Create Even or Odd.py

---
+++ Even or Odd.py
@@ -0,0 +1,6 @@
+#link this kata is : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+def even_or_odd(n):
+    if n % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
