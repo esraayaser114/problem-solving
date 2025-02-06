8e6a145c8385b742eddd0bba00cf5e327e9d66c6
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-02-06 13:39:02 +0200

    Delete Even or Odd.py

--- Even or Odd.py
+++
@@ -1,6 +0,0 @@
-#link this kata is : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
-def even_or_odd(n):
-    if n % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
