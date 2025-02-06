77013301e9c0bfb0552689d74e572a9dda7a7428
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-02-06 13:39:24 +0200

    Delete Is n divisible by x and y?.py

--- Is n divisible by x and y?.py
+++
@@ -1,7 +0,0 @@
-# link this kata is : https://www.codewars.com/kata/5545f109004975ea66000086/train/python
-def is_divisible(n,x,y):
-    if n % x == 0 and n % y == 0:
-        return True
-    else:
-        return False
-       
