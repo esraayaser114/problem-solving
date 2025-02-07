646ec780d9cfc48a7e5b350ce13a89ec59c4fdf7
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-02-07 13:39:12 +0200

    Update and rename Even or Odd.py to Even or Odd.cpp

--- CodeWars/C++/8kyu/Even or Odd.cpp
+++ CodeWars/C++/8kyu/Even or Odd.cpp
@@ -1,4 +1,4 @@
-# link this kataa is : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/cpp
+// link this kataa is : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/cpp
 #include <string>
 
 std::string even_or_odd(int number) 
