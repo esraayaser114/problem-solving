b3bf61702d1689dd093dad3389e248b7277d639a
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-02-07 13:47:37 +0200

    Create Reversed Strings.cpp

---
+++ CodeWars/C++/8kyu/Reversed Strings.cpp
@@ -0,0 +1,11 @@
+//this link is : https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/cpp
+#include <string>
+using namespace std ; 
+
+string reverseString (string str )
+{
+  reverse (str.begin(),str.end());
+  return str;
+
+
+}
