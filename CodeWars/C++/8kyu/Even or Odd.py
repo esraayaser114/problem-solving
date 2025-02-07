6283a8f7768b993c94f832b5d1e57d827370e7ea
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-02-07 13:38:43 +0200

    Create Even or Odd.py

---
+++ CodeWars/C++/8kyu/Even or Odd.py
@@ -0,0 +1,12 @@
+# link this kataa is : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/cpp
+#include <string>
+
+std::string even_or_odd(int number) 
+{
+  if (number % 2 == 0 ){
+    return "Even";
+  }
+  else {
+    return "Odd";
+  }
+}
