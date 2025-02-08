854c4f0b3181235c66a130d69a88d262ccba5caf
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-02-08 10:00:32 +0200

    Create Quarter of the year.py

---
+++ CodeWars/Python/8kyu/Quarter of the year.py
@@ -0,0 +1,15 @@
+#link this kata is : https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/python
+def quarter_of(m):
+    if m<=3:
+        return 1
+        
+    elif m<=6:
+        return  2
+        
+    elif m<=9:
+        return  3
+    else:
+        
+        return 4
+        
+    
