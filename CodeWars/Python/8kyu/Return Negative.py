c643129f59c1b6e52c756076eefdbe5b46ea6ff1
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-03-10 01:23:03 +0200

    Create Return Negative.py

---
+++ CodeWars/Python/8kyu/Return Negative.py
@@ -0,0 +1,8 @@
+# link this kata is : https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+def make_negative(n):
+    if n < 0:
+        return n
+    elif n == 0:
+        return 0
+    else:
+        return -n 
