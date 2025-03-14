554d7fa7f72db366311e6dc50e859fe2c2674540
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-03-14 20:36:58 +0200

    Create Is he gonna survive?.py

---
+++ CodeWars/Python/8kyu/Is he gonna survive?.py
@@ -0,0 +1,6 @@
+#link this kata is : https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/python
+def hero(bullets, dragons):
+    if(bullets>=dragons*2):
+        return True
+    else:
+        return False
