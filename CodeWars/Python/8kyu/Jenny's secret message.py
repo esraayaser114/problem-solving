f01795e7701450c72d8daba763bb3f24e851f603
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-03-16 14:29:52 +0200

    Create Jenny's secret message.py

---
+++ CodeWars/Python/8kyu/Jenny's secret message.py
@@ -0,0 +1,5 @@
+#link this kata is : https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/python
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)
