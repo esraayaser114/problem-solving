802a3825d0f16ad7b53e4e161c897223b03fe2c8
esraa_yaser <140348200+esraayaser114@users.noreply.github.com>
2025-09-01 01:18:15 +0300

    Create You only need one - Beginner.py

---
+++ CodeWars/Python/8kyu/You only need one - Beginner.py
@@ -0,0 +1,6 @@
+# link this kata is : https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/python
+def check(seq, elem):
+    if elem in seq:
+        return True
+    else:
+       return False
